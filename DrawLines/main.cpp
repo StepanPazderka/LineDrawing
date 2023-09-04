1638f6b790c5ecb3258fdb253eb944fe2b21177b
Stepan Pazderka <stepan.pazderka@icloud.com>
2023-09-04 14:33:19 +0200

    Initial Commit

---
+++ DrawLines/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DrawLines
+//
+//  Created by Štěpán Pazderka on 04.09.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
